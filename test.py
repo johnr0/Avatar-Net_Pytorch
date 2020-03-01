58b9b7c63578bf19da95da5a1b2147ef0b0c80ae
jjyc <johnr0hol@gmail.com>
2020-02-29 23:06:07 -0500

    server.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import torch
 
 from network import AvatarNet
-from utils import imload, imload_web imsave, maskload
+from utils import imload, imload_web, imsave, maskload
 
 
 def network_test(args):
